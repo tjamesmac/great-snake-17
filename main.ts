36b7878e86ade8cce67c608b8c1fdc7fdd540140
Thomas McAuliffe <tmcauliffe@roku.com>
2023-09-12 11:09:05 +0100

    remove json thing too

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@
 //
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
 
 const KEY = "dinosaur";
 const kv = await Deno.openKv();
