a4261a3b298b462a5b5fd7ff1e3f847403aebe2e
Thomas McAuliffe <tmcauliffe@roku.com>
2023-09-12 11:04:55 +0100

    go go go

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./data.json" assert { type: "json" };
 
 const KEY = "dinosaur";
-// const TEST = "Aardonyx";
 const kv = await Deno.openKv();
 
 const router = new Router();
@@ -16,12 +15,22 @@ router
   .get("/api", (context) => {
     context.response.body = data;
   })
+  .get('/api/dinos', async () => {
+    for (const dino of data) {
+      await kv.set([KEY, dino.name.toLowerCase()], dino)
+    }
+  })
   .get("/api/:dinosaur", async (context) => {
     if (context?.params?.dinosaur) {
-      const found = await kv.get([KEY, context?.params?.dinosaur]);
+      const found = await kv.get([
+        KEY,
+        context?.params?.dinosaur.toLowerCase(),
+      ]);
       if (found) {
+        console.log(found, "am i found");
         context.response.body = found.value;
       } else {
+        console.log(found, "am i not found");
         context.response.body = "No dinosaurs found.";
       }
     }
@@ -32,4 +41,5 @@ app.use(oakCors()); // Enable CORS for All Routes
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+console.log("running 🦕");
+await app.listen({ port: 8005 });
